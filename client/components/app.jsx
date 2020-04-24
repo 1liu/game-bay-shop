678dc5c91b684d154d4d606cc3e39265feb8b357
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-04-23 18:29:30 -0700

    View Product Details Front End

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
+      name: 'catalog',
+      params: {}
     };
+    this.setView = this.setView.bind(this);
   }
 
   componentDidMount() {
@@ -18,13 +20,29 @@ export default class App extends React.Component {
       .finally(() => this.setState({ isLoading: false }));
   }
 
+  setView(name, params) {
+    this.setState({
+      name: name,
+      params: params
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <Header />
-        <ProductList />
-      </div>
-    );
+    if (this.state.name === 'catalog') {
+      return (
+        <div>
+          <Header />
+          <ProductList setView={this.setView} />
+        </div>
+      );
+    } else if (this.state.name === 'details') {
+      return (
+        <div>
+          <Header />
+          <ProductDetails setView={this.setView} params={this.state.params}/>
+        </div>
+      );
+    }
 
   }
 }
