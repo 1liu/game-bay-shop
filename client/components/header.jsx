d1762ea72c50bd3c441621e3a9fbb22dca713311
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-19 15:36:59 -0700

    Added max width to header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 export default function Header(props) {
   return (
     <div className="navbar navbar-dark bg-dark justify-content-between m-auto sticky-top">
-      <h5 className="click header-text my-auto text-white" onClick={() => props.setView('catalog', {})}>
-        <i className="fas fa-dollar-sign"></i> Wicked Sales
-      </h5>
-      <h6 className="click my-auto mr-3 text-white" onClick={() => props.setView('cart', {})}>
-        {props.cartItemCount} items
-        <i className="fas fa-shopping-cart m-2"></i>
-      </h6>
+      <div className="container">
+        <h5 className="click header-text my-auto text-white" onClick={() => props.setView('catalog', {})}>
+          <i className="fas fa-dollar-sign"></i> Wicked Sales
+        </h5>
+        <h6 className="click my-auto mr-3 text-white" onClick={() => props.setView('cart', {})}>
+          {props.cartItemCount} items
+          <i className="fas fa-shopping-cart m-2"></i>
+        </h6>
+      </div>
     </div>
   );
 }
