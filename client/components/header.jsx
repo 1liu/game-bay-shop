e101e6c3babce6922177ad0955fb3dbfeb20f4a7
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-19 14:29:42 -0700

    Added cursor pointer style to cards and shopping cart

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,7 +13,7 @@ export default class Header extends React.Component {
         <div className="container">
           <nav className="navbar navbar-dark justify-content-between">
             <a className="navbar-brand" href="#"><i className="fas fa-dollar-sign"></i> Wicked Sales</a>
-            <div className="shoppingCartBtn" onClick={() => this.props.setView('cart', {})}>
+            <div className="shoppingCartBtn pointer" onClick={() => this.props.setView('cart', {})}>
               <p className='d-inline-block'>{this.props.cartItemCount} items</p>
               <i className="fas fa-shopping-cart"></i>
             </div>
