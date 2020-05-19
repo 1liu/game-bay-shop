8dc421d80d58821999a0b213825deacf12f0b0fb
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-19 15:36:28 -0700

    Footer Functional Component

---
+++ client/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer(props) {
+
+  return (
+    <div className="bg-dark text-white py-4">
+      <div className="container">
+        <h3 className="footer-text text-center pt-3">
+          <i className="fas fa-dollar-sign mr-2"></i>
+        Wicked Sales
+        </h3>
+        <p className="text-center pb-0">Happy Life with Wicked Sales</p>
+      </div>
+    </div>
+  );
+
+}
