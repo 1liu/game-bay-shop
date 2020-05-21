b0a0c5ce3048a3a2f73ce371ef3e6b04fcc5d124
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-20 18:10:27 -0700

    Converted to functional component

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-export default class CartSummaryItem extends React.Component {
+export default function CartSummaryItem(props) {
 
-  render() {
-    return (
-      <div className="card cart-item-row mb-3">
-        <div className="row no-gutters cart-item-row">
-          <div className="col-md-5">
-            <img src={this.props.product.image} className="card-img" alt="..."></img>
-          </div>
-          <div className="col-md-7">
-            <div className="card-body">
-              <h5 className="detail-title">{this.props.product.name}</h5>
-              <p className="detail-title price">${(this.props.product.price / 100).toFixed(2)}</p>
-              <p className="detail-text">{this.props.product.shortDescription}</p>
-            </div>
+  return (
+    <div className="card cart-item-row mb-3">
+      <div className="row no-gutters cart-item-row">
+        <div className="col-md-5">
+          <img src={props.product.image} className="card-img" alt="..."></img>
+        </div>
+        <div className="col-md-7">
+          <div className="card-body">
+            <h5 className="detail-title">{props.product.name}</h5>
+            <p className="detail-title price">${(props.product.price / 100).toFixed(2)}</p>
+            <p className="detail-text">{props.product.shortDescription}</p>
           </div>
         </div>
       </div>
-    );
+    </div>
 
-  }
+  );
 }
