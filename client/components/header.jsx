4c4e9f7810bd9515f0303d94161360933c2b897e
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-20 18:48:51 -0700

    added hover interaction

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,10 +4,10 @@ export default function Header(props) {
   return (
     <div className="navbar navbar-dark bg-dark justify-content-between m-auto sticky-top">
       <div className="container">
-        <h5 className="click header-text my-auto text-white" onClick={() => props.setView('catalog', {})}>
+        <h5 className="pointer header-text my-auto text-white" onClick={() => props.setView('catalog', {})}>
           <i className="fas fa-gamepad"></i> Game Bay Shop
         </h5>
-        <h6 className="click my-auto mr-3 text-white" onClick={() => props.setView('cart', {})}>
+        <h6 className="pointer my-auto mr-3 text-white" onClick={() => props.setView('cart', {})}>
           {props.cartItemCount} items
           <i className="fas fa-shopping-cart m-2"></i>
         </h6>
