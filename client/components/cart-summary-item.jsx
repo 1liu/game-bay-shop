00bd2488cb6b207eff55a8780726c728586163d9
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-05-21 22:19:50 -0700

    Conditionally render shortDescription based on screen height

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 
-export default function CartSummaryItem(props) {
+export default class CartSummaryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDesktop: window.innerHeight > 900
+    };
+  }
 
-  return (
-    <div className="card cart-item-row mb-3">
-      <div className="row no-gutters cart-item-row">
-        <div className="col-md-5">
-          <img src={props.product.image} className="card-img" alt="..."></img>
-        </div>
-        <div className="col-md-7">
-          <div className="card-body">
-            <h5 className="detail-title">{props.product.name}</h5>
-            <p className="detail-title price">${(props.product.price / 100).toFixed(2)}</p>
-            <p className="detail-text">{props.product.shortDescription}</p>
+  render() {
+    const isDesktop = this.state.isDesktop;
+    return (
+      <div className="card cart-item-row mb-3">
+        <div className="row no-gutters cart-item-row">
+          <div className="col-md-5">
+            <img src={this.props.product.image} className="card-img" alt="..."></img>
+          </div>
+          <div className="col-md-7">
+            <div className="card-body">
+              <h5 className="detail-title">{this.props.product.name}</h5>
+              <p className="detail-title price">${(this.props.product.price / 100).toFixed(2)}</p>
+              {isDesktop ? (
+                <p className="detail-text">{this.props.product.shortDescription}</p>
+              ) : null
+              }
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
